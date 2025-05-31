9ae513be854a992f542bd9dd7106b43d2a2b07c8
cRejon <carlosrejongomez@gmail.com>
2025-05-31 15:30:07 +0200

    Change camera to HSL instead HTTP

--- test/test_config.py
+++ test/test_config.py
@@ -5,7 +5,7 @@ class Config(object):
         'mountings': [{
             'id': '3', 
             'duration': 10, # minutes
-            'cam_url': 'http://ULR_TO_WEBCAM/Mjpeg',
+            'cam_url': 'https://ULR_TO_WEBCAM/stream.m3u8',
         },],
         'labs': [{
             'lab_name' : 'in4labs_robotics',
